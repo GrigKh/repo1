a51e3e4eeeae9d7ddf84a0f85a5370535cb08f6a
GrigKh <grigorcco@gmail.com>
2021-04-22 17:36:56 -0700

    java homework

---
+++ Main.java
@@ -0,0 +1,49 @@
+// Basic java program 
+// to print "Hello World"
+
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) 
+{
+double x = 15.0;
+int b = 4;
+
+if (x > b) {
+       // sdsdsds
+} else {
+      //ssssss
+}
+ x = x + 5;
+ x += 5;  
+
+int c = 7;
+x = c++;
+System.out.println(c);
+System.out.println(x);
+int k = 9;
+b = ++k;
+System.out.println(k);
+System.out.println(b);
+
+for (int i = 1; i < 21; ++i)
+System.out.println(i);
+
+int a = 10;
+
+if (a % 2 != 1)
+ 
+System.out.println("This number is even");
+          
+else 
+
+System.out.println("This number is odd");
+
+int w = 10;
+
+if (w % 2 == 0)
+
+System.out.println(w);
+
+}
+}
